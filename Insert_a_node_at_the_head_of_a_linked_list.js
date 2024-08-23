a07f6fc09ac37723e08714d0574d13a40f8f802a
chelsea-banke <chelseanewhorizon@gmail.com>
2024-08-23 20:15:07 +0100

    Insert a node at the head of a linked list complete

---
+++ Insert_a_node_at_the_head_of_a_linked_list.js
@@ -0,0 +1,15 @@
+// Complete the insertNodeAtHead function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function insertNodeAtHead(head, data) {
+    head = {data: data, next: head}
+    return head
+}
